3dac41a680ac37c40e2b01c361efd5d992f49545
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-10-31 09:14:40 +0100

    changes due to creating new app for projects

--- www_ReDesignIT_pl/urls.py
+++ www_ReDesignIT_pl/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('apps.home.urls')),
+    path('projects/', include('apps.projects.urls')),
     path('admin/', admin.site.urls),
 ]
