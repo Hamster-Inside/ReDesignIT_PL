2e3b3f6a5696e8ef1cdc5a7a0be38e6cab5f7424
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-01 23:37:18 +0100

    A bit of a frontend work. Index page work with interactive header

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Project
 
 
 # Create your views here.
 
 def projects(request):
-    template = loader.get_template('projects.html')
+    my_projects = Project.objects
+    template = loader.get_template('projects.html', {'Projects:': my_projects})
     return HttpResponse(template.render())
---
+++ static/js/header-sticky.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {stickFunction();}
+var header = document.getElementById("header-sticky");
+var sticky = header.offsetTop;
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickFunction() {
+
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
