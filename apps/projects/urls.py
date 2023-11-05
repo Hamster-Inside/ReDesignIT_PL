c84b8d668079323e9a6ad2b7b6ae9eab9ea37bb4
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-05 20:27:25 +0100

    Added project page with reference to project

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
+    path('projects/<int:pk>', views.project_detail, name='project-detail'),
 ]
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 from .models import Project
 
@@ -10,4 +10,14 @@ def projects(request):
     my_projects = Project.objects.all()
     template = 'projects.html'
     context = {'my_projects': my_projects}
+    return render(request, template, context)
+
+
+def project_detail(request, pk):
+    template = 'project-detail.html'
+    try:
+        picked_project = Project.objects.get(pk=pk)
+        context = {'project': picked_project}
+    except Project.DoesNotExist:
+        raise Http404("Project does not exist")
     return render(request, template, context)
