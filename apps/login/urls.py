0b5ff353ac7ccdefb25d4749577829a3b9b5d18a
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-10 12:12:09 +0100

    added working register, login, logout

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('login', views.login_request, name='login'),
+    path('login/', views.login_redirect, name='login_redirect'),
     path('register', views.register_request, name='register'),
+    path('register/', views.register_redirect, name='register_redirect'),
+    path("logout", views.logout_request, name="logout"),
 ]
--- apps/login/views.py
+++ apps/login/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
 
-def login(request, info='no info'):
-    template = 'login.html'
-    print(info)
-    context = {}
-    return render(request, template, context)
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+                return redirect("login")
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect("login")
+    form = AuthenticationForm()
+    return render(request=request, template_name="login.html", context={"login_form": form})
 
 
 def register_request(request):
@@ -20,5 +35,21 @@ def register_request(request):
             messages.success(request, "Registration successful.")
             return redirect("home")
         messages.error(request, "Unsuccessful registration. Invalid information.")
+        return redirect("register")
     form = NewUserForm()
     return render(request=request, template_name="register.html", context={"register_form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("home")
+
+
+# Added below function because of the firefox problem auto add slash after url and ends with 404
+def login_redirect(request):
+    return redirect("login")
+
+
+def register_redirect(request):
+    return redirect("register")
