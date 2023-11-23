50e4bd90882792911e179baa200b0f8277141307
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-23 20:47:48 +0100

    change home to class based views

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,23 +1,25 @@
 import logging
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.views.generic import TemplateView
+
 from .models import ProgrammingLanguage, Framework, WebProfile
 
-# Create your views here.
 logger = logging.getLogger(__name__)
 
 
-def home(request):
-    logger.info(f' | Browser: {request.user_agent.browser.family} | '
-                f'Version: {request.user_agent.browser.version_string} | '
-                f'Device: {request.user_agent.device} | '
-                f'OS: {request.user_agent.os.family} '
-                f'{request.user_agent.os.version_string}')
-    programming_languages = ProgrammingLanguage.objects.all()
-    frameworks = Framework.objects.all()
-    social_profiles = WebProfile.objects.all()
-    template = 'index.html'
-    context = {'programming_languages': programming_languages, 'frameworks': frameworks,
-               'social_profiles': social_profiles}
-    return render(request, template, context)
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['programming_languages'] = ProgrammingLanguage.objects.all()
+        context['frameworks'] = Framework.objects.all()
+        context['social_profiles'] = WebProfile.objects.all()
+
+        user_agent = self.request.user_agent
+        logger.info(f' | Browser: {user_agent.browser.family} | '
+                    f'Version: {user_agent.browser.version_string} | '
+                    f'Device: {user_agent.device} | '
+                    f'OS: {user_agent.os.family} '
+                    f'{user_agent.os.version_string}')
+
+        return context
