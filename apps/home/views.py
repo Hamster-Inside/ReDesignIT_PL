f191fc9302a15ed665dbb8b63e1acc9f3c6e61ff
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-11-30 19:56:42 +0100

    added django ipware to test ip catching

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,6 +2,7 @@ import logging
 from django.views.generic import TemplateView
 
 from .models import ProgrammingLanguage, Framework, WebProfile, MyApp
+from ipware import get_client_ip
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +17,18 @@ class HomeView(TemplateView):
         context['social_profiles'] = WebProfile.objects.all()
         context['apps'] = MyApp.objects.all()
 
+        client_ip, is_routable = get_client_ip(self.request)
+        if client_ip is None:
+            print('ip is none')  # Unable to get the client's IP address
+        else:
+            print(f'Client IP: {client_ip}')  # We got the client's IP address
+            if is_routable:
+                print('Clients IP is routable')  # The client's IP address is publicly routable on the Internet
+            else:
+                print('IP is private')  # The client's IP address is private
+
         user_agent = self.request.user_agent
+
         logger.info(f' | Browser: {user_agent.browser.family} | '
                     f'Version: {user_agent.browser.version_string} | '
                     f'Device: {user_agent.device} | '
