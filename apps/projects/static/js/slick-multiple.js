fcb822de8aac323388eafb1838370610e0c7d567
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-12-04 12:42:32 +0100

    added slick to projects

--- apps/projects/static/js/slick-multiple.js
+++ apps/projects/static/js/slick-multiple.js
@@ -1,5 +1,9 @@
 $('.multiple-items').slick({
+  dots: true,
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  centerMode: true,
 });
