d005050951a3f498b91cdf80baed7f0d14155f78
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-12-06 12:27:14 +0100

    added images for about page and working on bootstrap slider

--- apps/about/admin.py
+++ apps/about/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import About
+from .models import About, AboutImages
 
 # Register your models here.
 
 admin.site.register(About)
+admin.site.register(AboutImages)
--- apps/about/models.py
+++ apps/about/models.py
@@ -8,3 +8,12 @@ class About(models.Model):
 
     def __str__(self):
         return 'About Me'
+
+
+class AboutImages(models.Model):
+    title = models.CharField(max_length=100, default='Title', null=False, blank=False)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=False, blank=False)
+
+    def __str__(self):
+        return self.title
