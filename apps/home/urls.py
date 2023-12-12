271029d97ed7710ba1256b6f1ccccdc458d679fe
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-12-12 08:46:18 +0100

    added template and logic for statistic page for staff users

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, StatisticView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('statistic', StatisticView.as_view(), name='statistic')
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from .data_log import log_to_file
 from .models import ProgrammingLanguage, Framework, WebProfile, MyApp, Statistic
@@ -36,3 +38,13 @@ class HomeView(TemplateView):
         context['apps'] = MyApp.objects.all()
 
         return context
+
+
+class StatisticView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = 'statistic.html'
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def handle_no_permission(self):
+        return redirect('home')
