9e0ecf422857aa5f4df222c827bcf5b74645f4b1
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-12-12 10:17:09 +0100

    delete test line

--- apps/home/views.py
+++ apps/home/views.py
@@ -52,5 +52,4 @@ class StatisticView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['users_location_data'] = Statistic.objects.all()
-        print(context)
         return context
