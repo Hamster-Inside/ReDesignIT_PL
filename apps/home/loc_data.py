f4bec9515e504e9600319c08dd58c6ac8329df2a
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-12-12 17:12:49 +0100

    changed data location url to ipinfo because its more accurate and more entries / month

--- apps/home/loc_data.py
+++ apps/home/loc_data.py
@@ -6,8 +6,10 @@ from .data_log import simple_log
 
 def get_user_location_data(user_ip):
     load_dotenv()
-    api_key = getenv('IPSTACK_API_KEY')
-    api_url = f'http://api.ipstack.com/{user_ip}?access_key={api_key}'
+    # api_key = getenv('IPSTACK_API_KEY') # ipstack less accurate info
+    api_key = getenv('IPINFO_API_KEY')
+    # api_url = f'http://api.ipstack.com/{user_ip}?access_key={api_key}'
+    api_url = f'ipinfo.io/{user_ip}?token={api_key}'
     loc_data = {}
     try:
         response = requests.get(api_url)
