858841fb29b8545c357013009e2fbbd85fd58604
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2023-12-19 07:46:26 +0100

    added testuser login

---
+++ apps/login/static/js/login-as-testuser.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#login-as-test-user").click(function() {
+        $("#id_email").val("TestUser.ReDesignIT@gmail.com");
+        $("#id_password").val("Mandarynki45$45");
+        $("#login-form").submit();
+    });
+});
