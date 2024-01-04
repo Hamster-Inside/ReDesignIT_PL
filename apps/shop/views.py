13c17ea71e6f578cfcb5a14a080c3879ee64fa0f
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-01-04 20:18:43 +0100

    added logic to see in which category user currently is at the moment

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -16,6 +16,18 @@ class CategoryDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         category = self.get_object()
+
+        # Needed for list of categories where user is
+        breadcrumbs = []
+        current_category = category
+        while category:
+            if current_category is None:
+                break
+            breadcrumbs.insert(0, current_category)
+            current_category = current_category.parent
+        context['breadcrumbs'] = breadcrumbs
+
         descendants = category.get_descendants(include_self=True)
         context['products'] = Product.objects.filter(category__in=descendants)
+        print(context)
         return context
