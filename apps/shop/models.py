ab423a313e05f68f3a47b70d831d629d110cb3f9
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-01-14 18:39:21 +0100

    create common fields model for the same save method to get unidecoded name with unique pk

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -4,44 +4,34 @@ from mptt.models import MPTTModel, TreeForeignKey
 from unidecode import unidecode
 
 
-class Category(MPTTModel):
-    name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(unique=True, blank=True)
-    description = models.TextField(blank=True)
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
+class CommonFields(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True, default="")
+
+    # Add other common fields here
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        ascii_name = unidecode(self.name)
+        self.slug = f'{slugify(ascii_name)}-{self.pk}'
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Category(MPTTModel, CommonFields):
+    description = models.TextField(blank=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
 
     class MPTTMeta:
         order_insertion_by = ['level']
 
-    def save(self, *args, **kwargs):
-        # Set the slug to be the same as the name, using Django's slugify function
-        ascii_category_name = unidecode(self.name)
-        self.slug = slugify(ascii_category_name)
 
-        # Call the save method of the parent class (Model) to save the object
-        super().save(*args, **kwargs)
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
+class Product(CommonFields):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
-    slug = models.SlugField(unique=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            ascii_category_name = unidecode(self.name)
-            self.slug = slugify(ascii_category_name)
-        super().save(*args, **kwargs)
-        new_slug = f'{slugify(self.name)}-{self.pk}'
-        if self.slug != new_slug:
-            self.slug = new_slug
-            super().save(*args, **kwargs)
