4683d365ac0f7e8afb90b55f961aeadbed428e6b
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-01-15 19:14:34 +0100

    add product detail url

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ShopHomeView, CategoryDetailView
+from .views import ShopHomeView, CategoryDetailView, ProductDetailView
 
 urlpatterns = [
     path('shop/', ShopHomeView.as_view(), name='shop'),
-    path('shop/categories/<slug:slug>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('shop/categories/<slug:slug>/',
+         CategoryDetailView.as_view(),
+         name='category_detail'),
+    path('shop/categories/<slug:category_slug>/<slug:product_slug>/',
+         ProductDetailView.as_view(),
+         name='product_detail'),
 ]
