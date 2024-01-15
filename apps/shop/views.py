0c524493a44fe4918b7e780175c2b89ae8c29686
ReDesignIT <arkadiusz.budkowski.dev@gmail.com>
2024-01-15 19:14:59 +0100

    add product detail view

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from .models import Category, Product
 
@@ -40,3 +41,15 @@ class CategoryDetailView(DetailView):
         context['category_children'] = children
 
         return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+
+    def get_object(self, queryset=None):
+        category_slug = self.kwargs.get('category_slug')
+        product_slug = self.kwargs.get('product_slug')
+        category = get_object_or_404(Category, slug=category_slug)
+        return get_object_or_404(Product, category=category, slug=product_slug)
